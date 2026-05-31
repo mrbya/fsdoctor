60d51e2f5be8780308aa2c42ca21a22d569584bd
mrbya <v.toth.svk2@gmail.com>
2026-05-31 15:55:17 +0200

    feat(fe - ui): started storybook stories impl

---
+++ src/stories/StatusBadge.stories.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { StatusBadge } from "$lib/components";
+
+  const { Story } = defineMeta({
+    title: "Componnets/StatusBadge",
+    component: StatusBadge,
+    tags: ["autodocs"],
+    argTypes: {
+      tone: {
+        control: { type: "select" },
+        options: ["neutral", "success", "warning", "danger", "info"],
+      },
+    },
+  });
+</script>
+
+<Story name="Neutral">
+  <StatusBadge label="Neutral" />
+</Story>
+
+<Story name="Success">
+  <StatusBadge label="Healthy" tone="success" />
+</Story>
+
+<Story name="Warning">
+  <StatusBadge label="Warning status" tone="warning" />
+</Story>
+
+<Story name="Danger">
+  <StatusBadge label="Danger status" tone="danger" />
+</Story>
+
+<Story name="Info">
+  <StatusBadge label="Info status" tone="info" />
+</Story>
